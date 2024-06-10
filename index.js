7686f7eb28b663f71ebc1ee7966c8eaf0e0b0b83
Won Park <parkwon@sysnova.co.kr>
2024-06-10 14:43:53 +0900

    added comments

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ const PAGE_PER_SHOW = 10;
 let scanned = [];
 let page = 0;
 let keys = [];
+const NOT_MATCH_INCH = "etc";
 
 // UTIL
 function getInch(source) {
-  const valid = ["16", "22", "29", "42"];
-  const DEFULT_MSG = "n/a";
+  const INCH_VALID = {
+    B09: "1.6",
+    B19: "2.2",
+    B29: "2.9",
+    B69: "4.2",
+  };
 
-  if (!source || source.length < 2) {
-    return DEFULT_MSG;
+  if (!source || source.length != 12) {
+    return NOT_MATCH_INCH;
   }
-  let cut = source.substr(0, 2);
-  if (valid.indexOf(cut) >= 0) {
-    let c = cut.toString();
-    return `${c.charAt(0)}.${c.charAt(1)}"`;
+
+  let inch = source.slice(-4).slice(0, 3);
+  if (INCH_VALID[inch]) {
+    console.log("INCH_VALID[inch]", INCH_VALID[inch]);
+    return `${INCH_VALID[inch]}"`;
   }
-  return DEFULT_MSG;
+  return NOT_MATCH_INCH;
 }
 
 // SCAN
@@ -46,11 +52,13 @@ function updateTagCount() {
   let i22 = scanned.filter((x) => x.inch == '2.2"').length;
   let i29 = scanned.filter((x) => x.inch == '2.9"').length;
   let i42 = scanned.filter((x) => x.inch == '4.2"').length;
+  let ietc = scanned.filter((x) => x.inch == NOT_MATCH_INCH).length;
 
   document.getElementById("i16").innerHTML = i16;
   document.getElementById("i22").innerHTML = i22;
   document.getElementById("i29").innerHTML = i29;
   document.getElementById("i42").innerHTML = i42;
+  document.getElementById("ietc").innerHTML = ietc;
 }
 
 function updateTotalScanned() {
@@ -71,12 +79,26 @@ function updateList() {
 
   let drawBuffer = [];
   for (let item of showItems) {
-    drawBuffer.push(`<tr>
-      <th>${item.no}</th>
-      <td>${item.barcode}</td>
-      <td>${item.inch}</td>
-      <td>${item.date}</td>
-    </tr>`);
+    drawBuffer.push(`<tr class='error'>`);
+    drawBuffer.push(`<th>${item.no}</th>`);
+    if (item.barcode.length == 12) {
+      drawBuffer.push(`<td >${item.barcode}</td>`);
+    } else {
+      drawBuffer.push(
+        `<td class='underline decoration-4 text-red-600'>${item.barcode}</td>`
+      );
+    }
+
+    if (item.inch == NOT_MATCH_INCH) {
+      drawBuffer.push(
+        `<td class='underline decoration-4 text-red-600'>${item.inch}</td>`
+      );
+    } else {
+      drawBuffer.push(`<td >${item.inch}</td>`);
+    }
+
+    drawBuffer.push(`<td>${item.date}</td>`);
+    drawBuffer.push(`</tr>`);
   }
 
   document.getElementById("tbody").innerHTML = drawBuffer.join("");
